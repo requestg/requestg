54b312ca6756fc412f9f308d391c7e210784d8b2
requestg <lyj891916@gmail.com>
2025-04-29 20:41:39 +0300

    Update Time_Sajad.py

--- Time_Sajad.py
+++ Time_Sajad.py
@@ -1,7 +1,1 @@
-import os
-os.system('clear')
-from cfonts import render
-from colorama import Fore,Style
-n = render('Sajad', colors=['white', 'green'], align='center')
-print(n)
-print('The Encoded By Sajad-@f_g_d_6')
+import os;os.system('clear');print('The Encoded By Sajad-@f_g_d_6')
